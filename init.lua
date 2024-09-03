cc526bee0f8454511a45dfb037d670c94d4241d5
Italo Guajardo <iguajardo@smu.cl>
2024-09-02 21:37:41 -0400

    feat: add lualine

--- init.lua
+++ init.lua
@@ -1,16 +1,3 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
+require("vim-options")
 require("config.lazy")
-
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.wo.relativenumber = true
-vim.wo.number = true
-
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
 
