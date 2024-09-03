bedd8da502932430e5b765d2e9b10440deec7cba
Italo Guajardo <iguajardo@smu.cl>
2024-09-02 20:25:45 -0400

    chore: a little refactor for the config

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 require("config.lazy")
 
 vim.cmd("set expandtab")
@@ -10,12 +13,4 @@ vim.wo.number = true
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-local builtin = require('telescope.builtin')
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle right<CR>")
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {}) 
-vim.keymap.set('n', '<leader>fs', require("telescope").extensions.live_grep_args.live_grep_args, { noremap = true }) 
+
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,11 +10,17 @@ return {
   tag = '^0.1.8',
   config = function()
     local telescope = require("telescope")
+    local builtin = require('telescope.builtin')
 
     -- first setup telescope
     telescope.setup({
         -- your config
     })
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {}) 
+    vim.keymap.set('n', '<leader>fs', telescope.extensions.live_grep_args.live_grep_args, { noremap = true }) 
 
     -- then load the extension
     telescope.load_extension("live_grep_args")
