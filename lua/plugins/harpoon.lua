11f44bf5f01ac68ab8e7ebffaa264d4762b5cac9
Italo Guajardo <italog.penaloza@gmail.com>
2024-11-07 17:54:10 -0300

    zz after harpoon jump

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -11,11 +11,11 @@ return {
     vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-    vim.keymap.set("n", "<leader>j", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<leader>k", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<leader>l", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<leader>;", function() harpoon:list():select(4) end)
-    vim.keymap.set("n", "<leader>'", function() harpoon:list():select(5) end)
+    vim.keymap.set("n", "<leader>j", function() harpoon:list():select(1); vim.cmd("normal! zz") end)
+    vim.keymap.set("n", "<leader>k", function() harpoon:list():select(2); vim.cmd("normal! zz") end)
+    vim.keymap.set("n", "<leader>l", function() harpoon:list():select(3); vim.cmd("normal! zz") end)
+    vim.keymap.set("n", "<leader>;", function() harpoon:list():select(4); vim.cmd("normal! zz") end)
+    vim.keymap.set("n", "<leader>'", function() harpoon:list():select(5); vim.cmd("normal! zz") end)
 
     -- Toggle previous & next buffers stored within Harpoon list
     vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
