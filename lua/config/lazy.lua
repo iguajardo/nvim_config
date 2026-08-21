6c42a8e60ed32de08ff4f4b29b9ca29c2ea98e28
Italo Guajardo <italog.penaloza@gmail.com>
2026-08-21 00:19:11 -0400

    update versions and fix problems with that

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,23 +28,3 @@ require("lazy").setup({
     notify = false,
   },
 })
-
-require('import-cost').setup({
-  -- Filetypes to attach to
-  filetypes = {
-    'javascript',
-    'javascriptreact',
-    'typescript',
-    'typescriptreact',
-  },
-  format = {
-    -- Format string for bytes/kilobytes in virtual text
-    byte_format = '%.1fb',
-    kb_format = '%.1fk',
-    -- Virtual text format (remove second "%s" to ignore gzipped size)
-    virtual_text = '%s (gzipped: %s)',
-  },
-  -- Highlight of virtual text —
-  -- a highlight group to link to or table as specified by nvim_set_hl()
-  highlight = 'Comment',
-})
--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,94 +1,105 @@
 return {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v4.x',
-  dependencies = {
-    -- LSP Support
-    { 'neovim/nvim-lspconfig', version = "*" },           -- Required
-    { 'williamboman/mason.nvim' },         -- Optional
-    { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			-- Asignar capacidades por defecto para autocompletado (cmp)
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    -- Autocompletion
-    { 'hrsh7th/nvim-cmp' },   -- Required
-    { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+			-- Keymaps al adjuntar cualquier Servidor LSP
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(event)
+					local opts = { buffer = event.buf }
+					vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+					vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+					vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+					vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+					vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+					vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+					vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+					vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+					vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+					vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+				end,
+			})
 
-    -- Snippets
-    { 'L3MON4D3/LuaSnip' }, -- Required
-    { 'onsails/lspkind.nvim' },
-  },
-  config = function()
-    local lsp_zero = require('lsp-zero')
+			-- Configuraciones específicas de servidores con la API nativa vim.lsp.config
+			vim.lsp.config("kotlin_language_server", {
+				capabilities = capabilities,
+			})
 
-    local lsp_attach = function(client, bufnr)
-      local opts = { buffer = bufnr }
+			vim.lsp.config("clangd", {
+				capabilities = capabilities,
+				cmd = {
+					"clangd",
+					"--background-index",
+					"--clang-tidy",
+					"--completion-style=detailed",
+					"--header-insertion=never",
+				},
+			})
 
-      vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-      vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-      vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-      vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-      vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-      vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-      vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-      vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-      vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-      vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-    end
+			require("mason-lspconfig").setup({
+				handlers = {
+					function(server_name)
+						-- API moderna sin llamadas directas a lspconfig
+						vim.lsp.config(server_name, { capabilities = capabilities })
+						vim.lsp.enable(server_name)
+					end,
+				},
+			})
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
+			"onsails/lspkind.nvim",
+		},
+		config = function()
+			local cmp = require("cmp")
 
-    lsp_zero.extend_lspconfig({
-      sign_text = true,
-      lsp_attach = lsp_attach,
-      capabilities = require('cmp_nvim_lsp').default_capabilities(),
-    })
-
-    require('mason').setup({})
-    require('mason-lspconfig').setup({
-      handlers = {
-        function(server_name)
-          require('lspconfig')[server_name].setup({})
-        end,
-      },
-    })
-    require('lspconfig').kotlin_language_server.setup{}
-
-    local cmp = require('cmp')
-
-    cmp.setup({
-      window = {
-        completion = {
-          highlight = 'CmpItemMenu',
-        },
-        documentation = {
-          highlight = 'CmpNormal',
-        }
-      },
-      sources = {
-        { name = 'nvim_lsp' },
-      },
-      mapping = cmp.mapping.preset.insert({
-        -- `Enter` key to confirm completion
-        ['<CR>'] = cmp.mapping.confirm({ select = false }),
-
-        -- Ctrl+Space to trigger completion menu
-        ['<C-Space>'] = cmp.mapping.complete(),
-      }),
-      snippet = {
-        expand = function(args)
-          vim.snippet.expand(args.body)
-        end,
-
-      },
-      formatting = {
-        format = function(entry, vim_item)
-          if vim.tbl_contains({ 'path' }, entry.source.name) then
-            local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
-            if icon then
-              vim_item.kind = icon
-              vim_item.kind_hl_group = hl_group
-              return vim_item
-            end
-          end
-          return require('lspkind').cmp_format({ with_text = false })(entry, vim_item)
-        end
-      }
-    })
-  end
+			cmp.setup({
+				window = {
+					completion = { highlight = "CmpItemMenu" },
+					documentation = { highlight = "CmpNormal" },
+				},
+				sources = {
+					{ name = "nvim_lsp" },
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+					["<C-Space>"] = cmp.mapping.complete(),
+				}),
+				snippet = {
+					expand = function(args)
+						vim.snippet.expand(args.body)
+					end,
+				},
+				formatting = {
+					format = function(entry, vim_item)
+						if vim.tbl_contains({ "path" }, entry.source.name) then
+							local icon, hl_group = require("nvim-web-devicons").get_icon(entry:get_completion_item().label)
+							if icon then
+								vim_item.kind = icon
+								vim_item.kind_hl_group = hl_group
+								return vim_item
+							end
+						end
+						return require("lspkind").cmp_format({ with_text = false })(entry, vim_item)
+					end,
+				},
+			})
+		end,
+	},
 }
